431ce73dcb0d3f0e352d82d57440af4d045bf760
Thiago Lemos <tclemos@outlook.com>
2019-08-12 15:08:21 -0300

    dracula theme option

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
+import drac from './assets/dracula.svg'
 
 import Routes from './routes';
 
 function App() {
+
+  function themeChange(e) {
+    if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'dracula');
+    }
+    else {
+      document.documentElement.removeAttribute('data-theme');
+    }
+  }
+
   return (
-    <Routes />
+    <div className="app">
+      <div className="theme-switch-wrapper">
+        <img src={drac} alt="dracula logo" />
+        <label className="theme-switch">
+          <input type="checkbox" id="checkbox" onChange={themeChange} />
+          <div className="slider round"></div>
+        </label>
+      </div>
+      <Routes />
+    </div>
   );
 }
 
